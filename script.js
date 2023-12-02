0ac6acdacc9832e2002920747f1df88b7671461b
Ismailseddik <ismailahmed200101@gmail.com>
2023-12-02 21:09:22 +0200

    js trials

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
 // Function to implement search functionality
-function search() {
-    var titleInput = document.getElementById('book_title');
-    var genreInput = document.getElementById('book_genre');
-    var titleFilter = titleInput.value.toUpperCase();
-    var genreFilter = genreInput.value.toUpperCase();
-    var bookList = document.getElementsByClassName('box');
-  
-    // Loop through all book items and hide those that don't match the search query
-    for (var i = 0; i < bookList.length; i++) {
-      var title = bookList[i].getElementsByTagName('h3')[0].innerText.toUpperCase();
-      var genre = bookList[i].getAttribute('data-genre').toUpperCase();
-  
-      if ((title.indexOf(titleFilter) > -1 || genre.indexOf(genreFilter) > -1) || (titleFilter === '' && genreFilter === '')) {
-        bookList[i].style.display = '';
-      } else {
-        bookList[i].style.display = 'none';
-      }
-    }
-  }
-  
-  // Event listener for the search form submission
-  document.querySelector('.books_filter form').addEventListener('submit', function (e) {
-    e.preventDefault(); // Prevent form submission
-    search();
-  });
-  
-  // Event listener for the search inputs
-  document.getElementById('book_title').addEventListener('keyup', search);
-  document.getElementById('book_genre').addEventListener('keyup', search);
+document.addEventListener("DOMContentLoaded", function() {
+    // Place your JavaScript code here
+    // Add event listeners, manipulate DOM, etc.
+    // For example:
+
+    var searchButton = document.getElementById("search_button");
+    searchButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        // Perform search functionality
+        function search() {
+            var titleInput = document.getElementById('book_title');
+            var genreInput = document.getElementById('book_genre');
+            var titleFilter = titleInput.value.toUpperCase();
+            var genreFilter = genreInput.value.toUpperCase();
+            var bookList = document.getElementsByClassName('box');
+          
+            // Loop through all book items and hide those that don't match the search query
+            for (var i = 0; i < bookList.length; i++) {
+              var title = bookList[i].getElementsByTagName('h3')[0].innerText.toUpperCase();
+              var genre = bookList[i].getAttribute('data-genre').toUpperCase();
+          
+              if ((title.indexOf(titleFilter) > -1 || genre.indexOf(genreFilter) > -1) || (titleFilter === '' && genreFilter === '')) {
+                bookList[i].style.display = '';
+              } else {
+                bookList[i].style.display = 'none';
+              }
+            }
+          }
+          
+          // Event listener for the search form submission
+          document.querySelector('.books_filter form').addEventListener('submit', function (e) {
+            e.preventDefault(); // Prevent form submission
+            search();
+          });
+          
+          // Event listener for the search inputs
+    });
+});
